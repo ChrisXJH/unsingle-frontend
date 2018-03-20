f207f75b69651b36a92fe88ad3329fa3d2be00a7
ChrisXJH <j352xu@edu.uwaterloo.ca>
2018-03-19 21:29:08 -0400

    Improved search bar: added suggested events

---
+++ src/app/explore-panel/explore-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TabPanelComponent } from '../tab-panel/tab-panel.component';
+
+@Component({
+  selector: 'app-explore-panel',
+  templateUrl: './explore-panel.component.html',
+  styleUrls: ['./explore-panel.component.css']
+})
+export class ExplorePanelComponent implements TabPanelComponent {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
