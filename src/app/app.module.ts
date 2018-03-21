89cca949f15379ee1cf9ab35141de6ecf3cd9835
ChrisXJH <j352xu@edu.uwaterloo.ca>
2018-03-20 20:17:10 -0400

    remove nav-bar-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { TabPanelComponent } from './tab-panel/tab-panel.component';
 import { ExplorePanelComponent } from './explore-panel/explore-panel.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
